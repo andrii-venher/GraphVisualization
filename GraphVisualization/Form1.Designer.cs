7e379767cac4631a5023b15ce0245ed96a0d57bc
VengerAndrey <vengerandr@gmail.com>
2021-01-18 15:48:32 +0200

    initial commit

---
+++ GraphVisualization/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿
+namespace GraphVisualization
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.graphToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGraphToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.dataGridViewPoints = new System.Windows.Forms.DataGridView();
+            this.dataGridViewMatrix = new System.Windows.Forms.DataGridView();
+            this.labelPoints = new System.Windows.Forms.Label();
+            this.labelMatrix = new System.Windows.Forms.Label();
+            this.matrixToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.generateMatrixToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPoints)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewMatrix)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.graphToolStripMenuItem,
+            this.matrixToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(980, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // graphToolStripMenuItem
+            // 
+            this.graphToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGraphToolStripMenuItem,
+            this.clearToolStripMenuItem});
+            this.graphToolStripMenuItem.Name = "graphToolStripMenuItem";
+            this.graphToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.graphToolStripMenuItem.Text = "Graph";
+            // 
+            // newGraphToolStripMenuItem
+            // 
+            this.newGraphToolStripMenuItem.Name = "newGraphToolStripMenuItem";
+            this.newGraphToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.newGraphToolStripMenuItem.Text = "Generate";
+            this.newGraphToolStripMenuItem.Click += new System.EventHandler(this.newGraphToolStripMenuItem_Click);
+            // 
+            // clearToolStripMenuItem
+            // 
+            this.clearToolStripMenuItem.Name = "clearToolStripMenuItem";
+            this.clearToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.clearToolStripMenuItem.Text = "Clear";
+            this.clearToolStripMenuItem.Click += new System.EventHandler(this.clearToolStripMenuItem_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox.Location = new System.Drawing.Point(12, 27);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox.TabIndex = 1;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // dataGridViewPoints
+            // 
+            this.dataGridViewPoints.AllowUserToAddRows = false;
+            this.dataGridViewPoints.AllowUserToDeleteRows = false;
+            this.dataGridViewPoints.AllowUserToResizeColumns = false;
+            this.dataGridViewPoints.AllowUserToResizeRows = false;
+            this.dataGridViewPoints.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewPoints.Enabled = false;
+            this.dataGridViewPoints.Location = new System.Drawing.Point(420, 43);
+            this.dataGridViewPoints.Name = "dataGridViewPoints";
+            this.dataGridViewPoints.Size = new System.Drawing.Size(160, 384);
+            this.dataGridViewPoints.TabIndex = 2;
+            // 
+            // dataGridViewMatrix
+            // 
+            this.dataGridViewMatrix.AllowUserToAddRows = false;
+            this.dataGridViewMatrix.AllowUserToDeleteRows = false;
+            this.dataGridViewMatrix.AllowUserToResizeColumns = false;
+            this.dataGridViewMatrix.AllowUserToResizeRows = false;
+            this.dataGridViewMatrix.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewMatrix.Location = new System.Drawing.Point(586, 43);
+            this.dataGridViewMatrix.Name = "dataGridViewMatrix";
+            this.dataGridViewMatrix.Size = new System.Drawing.Size(384, 384);
+            this.dataGridViewMatrix.TabIndex = 3;
+            // 
+            // labelPoints
+            // 
+            this.labelPoints.AutoSize = true;
+            this.labelPoints.Location = new System.Drawing.Point(482, 27);
+            this.labelPoints.Name = "labelPoints";
+            this.labelPoints.Size = new System.Drawing.Size(36, 13);
+            this.labelPoints.TabIndex = 4;
+            this.labelPoints.Text = "Points";
+            // 
+            // labelMatrix
+            // 
+            this.labelMatrix.AutoSize = true;
+            this.labelMatrix.Location = new System.Drawing.Point(735, 27);
+            this.labelMatrix.Name = "labelMatrix";
+            this.labelMatrix.Size = new System.Drawing.Size(87, 13);
+            this.labelMatrix.TabIndex = 5;
+            this.labelMatrix.Text = "Adjacency matrix";
+            // 
+            // matrixToolStripMenuItem
+            // 
+            this.matrixToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.generateMatrixToolStripMenuItem});
+            this.matrixToolStripMenuItem.Name = "matrixToolStripMenuItem";
+            this.matrixToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.matrixToolStripMenuItem.Text = "Matrix";
+            // 
+            // generateMatrixToolStripMenuItem
+            // 
+            this.generateMatrixToolStripMenuItem.Name = "generateMatrixToolStripMenuItem";
+            this.generateMatrixToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.generateMatrixToolStripMenuItem.Text = "Generate";
+            this.generateMatrixToolStripMenuItem.Click += new System.EventHandler(this.generateMatrixToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(980, 439);
+            this.Controls.Add(this.labelMatrix);
+            this.Controls.Add(this.labelPoints);
+            this.Controls.Add(this.dataGridViewMatrix);
+            this.Controls.Add(this.dataGridViewPoints);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "GraphVisualization";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPoints)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewMatrix)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem graphToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newGraphToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.DataGridView dataGridViewPoints;
+        private System.Windows.Forms.DataGridView dataGridViewMatrix;
+        private System.Windows.Forms.Label labelPoints;
+        private System.Windows.Forms.Label labelMatrix;
+        private System.Windows.Forms.ToolStripMenuItem matrixToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem generateMatrixToolStripMenuItem;
+    }
+}
+
---
+++ GraphVisualization/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GraphVisualization
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap bitmap;
+        private List<Point> points = new List<Point>();
+        private Graphics graphics;
+
+        private const int Radius = 6;
+        private const int EdgeThickness = 2;
+        private const int PointLabelFontSize = 12;
+        private const int EdgeLabelFontSize = 10;
+        private const string FontName = "Arial";
+
+        public Form1()
+        {
+            InitializeComponent();
+            bitmap = new Bitmap(pictureBox.Width, pictureBox.Height);
+            graphics = Graphics.FromImage(bitmap);
+            dataGridViewPoints.Columns.Add("pointNumber", "№");
+            dataGridViewPoints.Columns.Add("pointСoordinates", "Point");
+            dataGridViewPoints.Columns[0].Width = 30;
+            dataGridViewPoints.Columns[1].Width = 70;
+            dataGridViewMatrix.RowHeadersWidth = 50;
+        }
+
+        private int EdgeLength(Point point1, Point point2)
+        {
+            return (int) Math.Sqrt(Math.Pow(point1.X - point2.X, 2) + Math.Pow(point1.Y - point2.Y, 2));
+        }
+
+        private void DrawPoint(Point point, string text)
+        {
+            graphics.FillEllipse(new SolidBrush(Color.Red),
+                point.X - Radius, point.Y - Radius, Radius * 2, Radius * 2);
+            graphics.DrawString(text, new Font(FontName, PointLabelFontSize),
+                new SolidBrush(Color.Black), point.X + Radius, point.Y + Radius);
+            pictureBox.Image = bitmap;
+        }
+
+        private void GenerateMatrix()
+        {
+            ClearMatrix();
+
+            for (int i = 0; i < points.Count; i++)
+            {
+                DataGridViewColumn column = new DataGridViewTextBoxColumn();
+                column.Name = $"{i}";
+                column.Width = 25;
+                dataGridViewMatrix.Columns.Add(column);
+            }
+
+            for (int i = 0; i < points.Count; i++)
+            {
+                dataGridViewMatrix.Rows.Add();
+                dataGridViewMatrix.Rows[i].HeaderCell.Value = $"{i}";
+            }
+
+            for (int i = 0; i < points.Count; i++)
+            {
+                for (int j = 0; j < points.Count; j++)
+                {
+                    dataGridViewMatrix.Rows[i].Cells[j].Value = (i < j) ? 1 : 0;
+                }
+            }
+
+            foreach (DataGridViewColumn column in dataGridViewMatrix.Columns)
+            {
+                column.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            }
+        }
+
+        private void DrawPoints()
+        {
+            for (int i = 0; i < points.Count; i++)
+            {
+                DrawPoint(points[i], $"{i}");
+            }
+        }
+
+        private void DrawEdges()
+        {
+            Point p1 = new Point();
+            Point p2 = new Point();
+            for (int i = 0; i < points.Count; i++)
+            {
+                for (int j = 0; j < points.Count; j++)
+                {
+                    if (i != j && Convert.ToInt32(dataGridViewMatrix.Rows[i].Cells[j].Value) == 1)
+                    {
+                        p1.X = points[i].X;
+                        p1.Y = points[i].Y;
+                        p2.X = points[j].X;
+                        p2.Y = points[j].Y;
+                        var label = EdgeLength(p1, p2).ToString();
+                        var font = new Font(FontName, EdgeLabelFontSize);
+                        var size = graphics.MeasureString(label, font, pictureBox.Size);
+                        graphics.DrawLine(new Pen(Color.Chartreuse, EdgeThickness), p1, p2);
+                        graphics.DrawString(label, 
+                            font, new SolidBrush(Color.Black), 
+                            (p1.X + p2.X) / 2 - size.Width / 2, (p1.Y + p2.Y) / 2 - size.Height / 2);
+                    }
+                }
+            }
+        }
+
+        private void ClearGraph()
+        {
+            graphics.Clear(Color.White);
+            pictureBox.Image = bitmap;
+            points.Clear();
+            dataGridViewPoints.Rows.Clear();
+        }
+
+        private void ClearMatrix()
+        {
+            dataGridViewMatrix.Rows.Clear();
+            dataGridViewMatrix.Columns.Clear();
+        }
+
+        private void pictureBox_Click(object sender, EventArgs e)
+        {
+            int x = MousePosition.X - Location.X - pictureBox.Location.X - 8;
+            int y = MousePosition.Y - Location.Y - pictureBox.Location.Y - 32;
+            points.Add(new Point(x, y));
+            dataGridViewPoints.Rows.Add(dataGridViewPoints.Rows.Count, $"({x}; {y})");
+            DrawPoint(points[points.Count - 1], $"{points.Count - 1}");
+        }
+
+        private void newGraphToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (dataGridViewMatrix.Rows.Count < dataGridViewPoints.Rows.Count)
+                GenerateMatrix();
+            DrawEdges();
+            Invalidate();
+        }
+
+        private void clearToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ClearGraph();
+            ClearMatrix();
+        }
+
+        private void generateMatrixToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            GenerateMatrix();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            graphics.Clear(Color.White);
+            pictureBox.Image = bitmap;
+            DrawPoints();
+            DrawEdges();
+            base.OnPaint(e);
+        }
+    }
+}
